b52a374119899650d5c5ffae18b5183554356053
Tristan Pendergrass <tristanp@dropbox.com>
2024-05-13 20:31:48 -0700

    Try to fix drag and drop controls

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import { Elm } from "./Main.elm";
 const app = Elm.Main.init({
   node: document.querySelector("main")
 });
+
+// Prevent double tap zoom
+let lastTouchEnd = 0;
+
+document.addEventListener('touchend', (event) => {
+  const now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
